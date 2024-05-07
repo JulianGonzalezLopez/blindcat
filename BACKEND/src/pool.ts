e63eeb6998233ad316d6c734de3c0c40654404f9
= <=>
2024-05-07 09:23:37 -0300

    fix: en vez de otorgar el objeto de clase pool (shortcut)

--- BACKEND/src/pool.ts
+++ BACKEND/src/pool.ts
@@ -5,21 +5,29 @@ dotenv.config();
 
 let pool : Pool | undefined;
 
-try{
-    pool = mysql.createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        waitForConnections: true,
-        connectionLimit: 100,
-        maxIdle: 10,
-        idleTimeout: 60000
-      });
-}
-catch(e){
-    console.log(e);
-    pool = undefined;
+async function getConnection(){
+    try{
+        pool = mysql.createPool({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            waitForConnections: true,
+            connectionLimit: 100,
+            maxIdle: 10,
+            idleTimeout: 60000
+          });
+          
+        let connection = await pool.getConnection();
+        
+        return connection;
+          
+    }
+    catch(e){
+        console.log(e);
+        pool = undefined;
+    }
+    
 }
 
 export default pool;
