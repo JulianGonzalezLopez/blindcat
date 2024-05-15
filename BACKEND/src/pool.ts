1e3bf29d18edccc066b9edea95cf9867356ad710
= <=>
2024-05-15 11:49:29 -0300

    CREO QUE AHÍ ANDAN SIN LAS EXTENSIONES

--- BACKEND/src/pool.ts
+++ BACKEND/src/pool.ts
@@ -5,6 +5,18 @@ dotenv.config();
 
 let pool : Pool | undefined;
 
+pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 100,
+    maxIdle: 10,
+    idleTimeout: 60000
+  });
+
+
 async function getConnection(){
     try{
         pool = mysql.createPool({
