6b73b893718c405401909829433bf0ba4f901e36
= <=>
2024-07-12 13:29:48 -0300

    Aclaracion de errores

--- BACKEND/src/helpers/ErrorSenderHelper.ts
+++ BACKEND/src/helpers/ErrorSenderHelper.ts
@@ -1,17 +1,18 @@
 import { Response } from "express";
 
 export default function handleError(res: Response, e: Object | unknown) {
+  console.log(e);
   if (typeof e === "object" && e !== null) {
     if ("statusCode" in e && typeof e.statusCode === "number") {
       if ("errorMessage" in e && typeof e.errorMessage === "string") {
-        res.status(e.statusCode).send(e.errorMessage);
+        res.status(e.statusCode).json({error:e.errorMessage});
       } else {
-        res.status(e.statusCode).send("Default error");
+        res.status(e.statusCode).json("Default error");
       }
     } else {
-      res.status(500).send(res || e);
+      res.status(500).json({error:res || e});
     }
   } else {
-    res.status(500).send(res || e);
+    res.status(500).json({error:res || e});
   }
 }
